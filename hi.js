a7f2cb66877a4319dd296608e7b149ec947e7e60
Josttin Mendez <justinstebanmendez@gmail.com>
2023-06-10 02:55:41 -0500

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,3 +1,26 @@
-cosole.log("hi")
-cosole.log("hi")
+const greeting = `
+   _____ _       _     _______ _             
+  / ____(_)     | |   |__   __| |            
+ | (___  _ _ __ | |_     | |  | |__   ___  __
+  \___ \| | '_ \| __|    | |  | '_ \ / _ \/ /
+  ____) | | | | | |_     | |  | | | |  __/| |
+ |_____/|_|_| |_|\__|    |_|  |_| |_|\___||_|
+`;
+
+function animateASCII(text, speed) {
+  const lines = text.split('\n');
+  let index = 0;
+
+  setInterval(() => {
+    console.clear();
+    console.log(lines[index]);
+
+    index++;
+    if (index === lines.length) {
+      index = 0;
+    }
+  }, speed);
+}
+
+animateASCII(greeting, 200);
 
